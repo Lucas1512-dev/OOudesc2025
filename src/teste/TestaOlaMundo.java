b6672435b83ee1d2933f5b204cc4db2e22e1edd4
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-03-31 21:44:56 -0300

    adicionar scanner

--- src/teste/TestaOlaMundo.java
+++ src/teste/TestaOlaMundo.java
@@ -1,11 +1,25 @@
 package teste;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         //<nome classe> <nome do objeto> new <nome classe>//
         OlaMundo xyz = new OlaMundo();
         xyz.imprimirDados();
         xyz.imprimir("Lucas", 43);
+
+        String nome;
+
+        Scanner leDados = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        nome = leDados.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int valor = leDados.nextInt();
+
+        xyz.imprimir(nome, valor);
 
     }
 }
